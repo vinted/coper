4e96915ac3d02533edfd7e805586c0a5eaf91099
Lukas Samuolis <lukas.samuolis@vinted.com>
2021-02-05 17:10:48 +0200

    Remove not needed lines

--- example/src/main/java/com/vinted/coper/example/ExampleSelectFragment.kt
+++ example/src/main/java/com/vinted/coper/example/ExampleSelectFragment.kt
@@ -1,34 +1,13 @@
 package com.vinted.coper.example
 
-import android.Manifest
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vinted.coper.CoperBuilder
 import kotlinx.android.synthetic.main.fragment_example_select.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class ExampleSelectFragment : Fragment() {
-
-    private val allowed by lazy {
-        CoperBuilder()
-            .setFragmentActivity(requireActivity())
-            .build()
-            .apply {
-                GlobalScope.launch {
-                    isPermissionsGrantedSafe(Manifest.permission.CAMERA)
-                }
-            }
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        allowed
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
