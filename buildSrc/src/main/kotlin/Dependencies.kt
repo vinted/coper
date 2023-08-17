c784179bbd9119934d587fad867fe23f70aa2777
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-08-17 13:31:38 +0300

    Bump Kotlin to 1.9.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val APP_COMPAT = "1.1.0"
-    const val KOTLIN = "1.8.0"
+    const val KOTLIN = "1.9.0"
     const val COROUTINES = "1.5.0"
     const val ANDROID_GRADLE_PLUGIN = "7.4.2"
     const val JUNIT = "4.13"
--- example/src/main/java/com/vinted/coper/example/PermissionExampleBusinessSideViewModelFactory.kt
+++ example/src/main/java/com/vinted/coper/example/PermissionExampleBusinessSideViewModelFactory.kt
@@ -7,7 +7,7 @@ import com.vinted.coper.Coper
 class PermissionExampleBusinessSideViewModelFactory(
     private val coper: Coper
 ): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(PermissionExampleInBusinessSideViewModel::class.java)) {
             PermissionExampleInBusinessSideViewModel(coper) as T
         } else {
